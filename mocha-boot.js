95ae75ec5acd97d8f8500c70a48fad5e1f74adce
Andrey Peresleguine <peresleguine@gmail.com>
2015-12-08 14:25:39 +0300

    updated catcher test

--- mocha-boot.js
+++ mocha-boot.js
@@ -1,4 +1,6 @@
 require('babel/register');
+require('./models').sequelize.sync({ force: true });
 process.env.POSTMARK_API_KEY = '9dd395f7-eae0-4cdc-b31f-d9ce4bb31852';
 process.env.REDIS_URL = 'redis://127.0.0.1:6379/1';
 process.env.WEBAPP_CLEARDB_DATABASE_URL = 'mysql://root@localhost/mentor_development';
+process.env.NODE_ENV = 'test'
--- test/test.js
+++ test/test.js
@@ -1,35 +1,33 @@
+import should from 'should'
 import { worker, queue } from '../clients'
 
 describe('Workers', () => {
+  beforeEach((done) => {
+    worker.connect(() => {
+      queue.connect(done)
+    })
+  })
+
+  afterEach((done) => {
+    worker.end(done)
+  })
 
   describe('catcher', () => {
-    beforeEach((done) => {
-      worker.connect(() => {
-        queue.connect(() => {
-          done()
-        })
-      })
-    })
+    it('should be skipped if user do not want to receive notifications', (done) => {
+      worker.on('success', (q, job, result) => {
+        try {
+          result.should.equal(true)
+        } catch (e) {
+          return done(e)
+        } finally {
+          worker.removeAllListeners('success')
+        }
 
-    afterEach((done) => {
-      worker.end()
-      done()
-    })
-
-    it('should properly enqueue spreader', (done) => {
-      worker.on('success', function(queue, job, result){
-        // queue.should.equal()
-        // job.class.should.equal()
-        // result.should.equal()
-        console.log(3)
-        worker.removeAllListeners('success')
         done()
       })
 
       queue.enqueue('notifications', 'catcher', '7f9d8b54-2f30-437d-9f8c-6c3ee503f47c')
-      console.log(1)
       worker.start()
-      console.log(2)
     })
   })
 
